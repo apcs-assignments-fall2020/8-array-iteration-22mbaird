35c67cd14d16222ac8eac184b2379aa695db934a
22mbaird <22mbaird@athenian.org>
2020-10-18 06:18:52 +0000

    Second attempt

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,79 +1,61 @@
-import java.util.Scanner;
 public class MyMain {
 
     // Reverses an array
-    public static int[] reverse(int[] arr) {
-        for (int i = 0; i < arr.length/2; i++){
-            int x = arr[i];
-            arr[i] = arr[arr.length - i - 1];
-            arr[arr.length - i - 1] = x;
+    public static int[] reverse(final int[] arr) {
+        final int[] rlist = new int[arr.length];
+        for (int i = 0; i < arr.length-1; i++) {
+            rlist[i] = arr[arr.length - 1 - i];
         }
-        return arr;
+        return rlist;
     }
 
     // Finds the second largest number in an array
-    public static int secondLargest(int[] arr) {
-        for (int i = 0; i < arr.length; i++){
-            for (int k = i+1; k < arr.length; k++){
-                if (arr[k] > arr[i]){
-                   int x = arr[i];
-                   arr[i] = arr[k];
-                   arr[k] = x;
+    public static int secondLargest(final int[] arr) {
+        for (int x = 0; x < arr.length; x++) {
+            int max = arr[0];
+            int maxi = 0;
+            for (int i = 0; i < arr.length-x; i++) {
+                if (arr[i] > max) {
+                max = arr[i];
+                maxi = i;
                 }
             }
+            int nmax = max;
+            arr[maxi] = arr[arr.length-1-x];
+            arr[arr.length-1-x] = nmax;
+            
+
         }
-        return arr[arr.length - 2];
+        int sb = arr[arr.length-1];
+        return sb;
+
+        
+
     }
 
     // Checks to see if an array contains a geometric series
-    public static boolean isGeometric(int[] arr) {
-        double[] factor = new double[arr.length-1];
-        for (int i = 0; i < arr.length; i++){
-            factor[i] = Double.valueOf(arr[i+1])/Double.valueOf(arr[i]);
+    public static boolean isGeometric(final int[] arr) {
+        final double[] factor = new double[arr.length - 1];
+        for (int i = 0; i < arr.length - 1; i++) {
+            factor[i] = Double.valueOf(arr[i + 1]) / Double.valueOf(arr[i]);
         }
         int c = 0;
-        for (int i= 1; i < factor.length; i++){
-             if (factor[i] == factor[i-1]){
-                c +=1; 
+        for (int i = 1; i < factor.length; i++) {
+            if (factor[i] == factor[i - 1]) {
+                c += 1;
             }
         }
-        
-        if (c == factor.length -1){
+
+        if (c == factor.length - 1) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
-    }   
+    }
 
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("How many values: ");
-        int num1 = scan.nextInt();
-        int arr_1[] =  new int[num1];
-        System.out.println("Enter all values: ");
-        for (int i = 0; i < num1; i++){
-            arr_1[i] = scan.nextInt();
-        }
-        System.out.println(reverse(arr_1));
-        System.out.print("How many values: ");
-        int num2 = scan.nextInt();
-        int arr_2[] =  new int[num2];
-        System.out.println("Enter all values: ");
-        for (int i = 0; i < num2; i++){
-            arr_2[i] = scan.nextInt();
-        }
-        System.out.println(secondLargest(arr_2));
-        System.out.print("How many values: ");
-        int num3 = scan.nextInt();
-        int arr_3[] =  new int[num3];
-        System.out.println("Enter all values: ");
-        for (int i = 0; i < num3; i++){
-            arr_3[i] = scan.nextInt();
-        }
-        System.out.println(isGeometric(arr_3));
-        
-        scan.close();
-    } 
+    public static void main(final String[] args) {
+        System.out.println(reverse(null));
+        System.out.println(secondLargest(null));
+        System.out.println(isGeometric(null));
+    }
 }
